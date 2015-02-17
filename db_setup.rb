c74ba9ebab46b2b86e7565e129a68f2a07c07a38
Ruthie Peters <ruthie_peters@yahoo.com>
2015-02-17 13:08:40 -0600

    search all plants works

--- db_setup.rb
+++ db_setup.rb
@@ -3,29 +3,46 @@ DATABASE.results_as_hash = true
 DATABASE.execute("CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY, name TEXT NOT NULL)")
 
 DATABASE.execute("CREATE TABLE IF NOT EXISTS plants (id INTEGER PRIMARY KEY,
-                  name TEXT NOT NULL UNIQUE, sun TEXT, more_info TEXT, picture TEXT, 
-                  category_id INTEGER, FOREIGN_KEY(category_id) REFERENCES categories(id))")
-                  
+                  name TEXT NOT NULL, sun TEXT, more_info TEXT, picture TEXT,
+                  category_id INTEGER, FOREIGN KEY(category_id) REFERENCES categories(id))")
+
 DATABASE.execute("CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY,
                  name TEXT NOT NULL)")
-                 
+
 DATABASE.execute("CREATE TABLE IF NOT EXISTS gardens (id INTEGER PRIMARY KEY,
                  name TEXT NOT NULL)")
-                  
-DATABASE.execute("CREATE TABLE IF NOT EXISTS selections (id INTEGER PRIMARY KEY, 
+
+DATABASE.execute("CREATE TABLE IF NOT EXISTS selections (id INTEGER PRIMARY KEY,
                   plant_id INTEGER, garden_id INTEGER,
                   FOREIGN KEY(plant_id) REFERENCES plants(id),
                   FOREIGN KEY(garden_id) REFERENCES gardens(id))")
-                  
-DATABASE.execute("CREATE TABLE IF NOT EXISTS special_notes (id INTEGER PRIMARY KEY, 
+
+DATABASE.execute("CREATE TABLE IF NOT EXISTS special_notes (id INTEGER PRIMARY KEY,
                   plant_id INTEGER, comment_id INTEGER,
                   FOREIGN KEY(plant_id) REFERENCES plants(id),
-                  FOREIGN KEY(comment_id) REFERENCES comments(id))")                
-                  
-                  
-if DATABASE.execute("SELECT * FROM categories") == []              
+                  FOREIGN KEY(comment_id) REFERENCES comments(id))")
+
+DATABASE.execute("CREATE TABLE IF NOT EXISTS search (id INTEGER PRIMARY KEY,
+                  name TEXT NOT NULL)")
+
+
+if DATABASE.execute("SELECT * FROM categories") == []
   categories = ["Ground Cover", "Front", "Middle", "Back"]
   categories.each do|category|
     DATABASE.execute("INSERT INTO categories (name) VALUES ('#{category}')")
   end
 end
+
+if DATABASE.execute("SELECT * FROM plants") == []
+    DATABASE.execute("INSERT INTO plants (name, sun, more_info, picture, category_id)
+    VALUES 
+    ('Foxglove', 'Part Shade', 'link', 'picture', 4),
+    ('Creeping Phlox', 'Full Sun', 'link', 'picture', 1),
+    ('Thyme', 'Full Sun', 'link', 'picture', 1),
+    ('Angelina Sedum', 'Full Sun', 'link', 'picture', 1),
+    ('Cornflower', 'Full Sun', 'link', 'picture', 4)")
+    
+ end
+    # "en.wikipedia.org/wiki/Digitalis",
+ #    "http://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Digitalis_purpurea2.jpg/220px-Digitalis_purpurea2.jpg",
+ #    4)")
--- main.rb
+++ main.rb
@@ -1,15 +1,16 @@
-require "sinatra"
+require 'sinatra'
 require 'pry'
 require 'sqlite3'
 DATABASE = SQLite3::Database.new('garden_selector.db')
-# require_relative "db_setup"
-# require_relative "database_methods"
-# require_relative 'plant'
-# require_relative 'comment'
-# require_relative 'category'
-# require_relative "garden"
-# require_relative 'selection'
-# require_relative 'special_note'
+require_relative 'db_setup'
+require_relative 'database_methods'
+require_relative 'models/category'
+require_relative 'models/plant'
+require_relative 'models/comment'
+require_relative 'models/search'
+require_relative 'models/garden'
+require_relative 'models/selection'
+require_relative 'models/special_note'
 
 
 # include Helper
@@ -19,4 +20,9 @@ DATABASE = SQLite3::Database.new('garden_selector.db')
 
 get "/" do
   erb :welcome, :layout=> :boilerplate
+end
+
+get "/plant_search"  do
+  @plant = Plant.all(params["table"])
+  erb :search_plants, :layout=> :boilerplate
 end
