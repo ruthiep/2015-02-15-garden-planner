bf63bf021b1636eda4a1b1f8390a2a9376336512
Ruthie <ruthie_peters@yahoo.com>
2019-05-23 12:02:39 -0500

    Update send_sms.rb

--- send_sms.rb
+++ send_sms.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'twilio-ruby'
 
 
-account_sid = "AC0f8ce07b36ea806570dc7948778a6437"
-auth_token = "9ec2512fea25ee73ddfc0f4556a12a8b"
+account_sid = ""
+auth_token = ""
 
 @client = Twilio::REST::Client.new account_sid, auth_token
 
